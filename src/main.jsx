1114d1bb824ebc0b8a5c16eb75386132ef5cecca
John Boyle <nikolasboyle@gmail.com>
2023-09-13 10:54:53 -0700

    replace BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "styled-components";
@@ -16,12 +16,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <HashRouter>
           <ThemeProvider theme={theme}>
             <GlobalStyle />
             <App />
           </ThemeProvider>
-        </BrowserRouter>
+        </HashRouter>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </AuthProvider>
